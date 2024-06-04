7ca844a27231c8dbb41a947ccf0d081d607d3c5b
kyhlsd <kmyghn@gmail.com>
2024-06-04 15:14:32 +0900

    BookmarkTableViewController, sampledata tableView 구현

---
+++ YGGG/YGGG/BookmarkTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  BookmarkTableViewController.swift
+//  YGGG
+//
+//  Created by 김영훈 on 6/4/24.
+//
+
+import UIKit
+
+class BookmarkTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    var datas = UserInfoEntry.sampleDatas
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(BookmarkTableViewCell.self, forCellReuseIdentifier: "bookmarkCell")
+        
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 24),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -24),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor)
+        ])
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        datas.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bookmarkCell", for: indexPath) as! BookmarkTableViewCell
+        cell.selectionStyle = .none
+        let userInfoEntry = datas[indexPath.row]
+        cell.configureCell(userInfoEntry: userInfoEntry)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        87
+    }
+    
+    
+}
+
