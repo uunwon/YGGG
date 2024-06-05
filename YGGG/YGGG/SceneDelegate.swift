8445f22b3c7e7513ffaf79efa0d8137605fe765a
uunwon <chea2089@naver.com>
2024-06-05 10:47:31 +0900

    Init] Dev Branch

--- YGGG/YGGG/SceneDelegate.swift
+++ YGGG/YGGG/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let navi = UINavigationController(rootViewController: ViewController())
+        let navi = UINavigationController(rootViewController: LoginViewController())
         
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = navi
