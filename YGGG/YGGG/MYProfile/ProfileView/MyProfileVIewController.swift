1d117546e86eb35a610b7f02ef9282dabf40b3de
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-06-11 16:51:41 +0900

    Add] MyProfileViewModel

--- YGGG/YGGG/MYProfile/ProfileView/MyProfileVIewController.swift
+++ YGGG/YGGG/MYProfile/ProfileView/MyProfileVIewController.swift
@@ -6,15 +6,11 @@
 //
 
 import UIKit
-import Firebase
-import GoogleSignIn
-import FirebaseAuth
 
 class MyProfileVIewController: UIViewController{
-
     
     private let mainProfileView = ProfileMainView()
-    private let auth = Auth.auth().currentUser
+    private let viewModel = MyProfileViewModel()
     
     private lazy var settingTableView: UITableView = {
         let tv = UITableView()
@@ -32,8 +28,8 @@ class MyProfileVIewController: UIViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureUI()
-        configureDataSetup()
+        setupUI()
+        setupData()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -41,14 +37,14 @@ class MyProfileVIewController: UIViewController{
         self.navigationController?.setNavigationBarHidden(true, animated: animated)
     }
     
-    private func configureUI() {
+    private func setupUI() {
         view.backgroundColor = .white
         view.addSubview(mainProfileView)
         
         mainProfileView.delegate = self
         
         mainProfileView.translatesAutoresizingMaskIntoConstraints = false
-       
+        
         [mainProfileView, settingTableView].forEach {
             view.addSubview($0)
         }
@@ -69,70 +65,12 @@ class MyProfileVIewController: UIViewController{
     }
     
     
-    private func configureDataSetup()  {
-        
-        if let _ = self.auth {
-            getData { user in
-                self.mainProfileView.setupUI(userImage: user.userImage, userName: user.userName,
-                                        tombCount: user.tombCount, refrigeratorCount: user.refrigeratorCount,
-                                        hashTag: user.email, isMyProfile: true)
-                
-            }
-        }
-    }
-    
-    
-    private func attributeButtonText(title: String, count: Int) -> NSAttributedString{
-        let normalAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 10)
-        ]
-        let boldAttributes: [NSAttributedString.Key: Any] = [
-            .font: UIFont.boldSystemFont(ofSize: 10)
-        ]
-        
-        let attributedString = NSMutableAttributedString(string: title, attributes: normalAttributes)
-        let countString = NSAttributedString(string: "\(count)", attributes: boldAttributes)
-        
-        attributedString.append(countString)
-        return attributedString
-    }
-    
-    func getData(completion: @escaping(User) -> Void) {
-        guard let uid = auth?.uid else { return }
-        RED_USERS.document(uid).getDocument { (document, error) in
-            if let document = document, document.exists {
-                do {
-                    if var data = document.data() {
-                        // Convert FIRTimestamp to formatted date string
-                        let dateFormatter = DateFormatter()
-                        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-                        
-                        if let userCosmetics = data["userCosmetics"] as? [[String: Any]] {
-                            var updatedUserCosmetics = [[String: Any]]()
-                            for var cosmetic in userCosmetics {
-                                if let expirationDate = cosmetic["expirationDate"] as? Timestamp {
-                                    cosmetic["expirationDate"] = dateFormatter.string(from: expirationDate.dateValue())
-                                }
-                                if let purchaseDate = cosmetic["purchaseDate"] as? Timestamp {
-                                    cosmetic["purchaseDate"] = dateFormatter.string(from: purchaseDate.dateValue())
-                                }
-                                updatedUserCosmetics.append(cosmetic)
-                            }
-                            data["userCosmetics"] = updatedUserCosmetics
-                        }
-                        
-                        let jsonData = try JSONSerialization.data(withJSONObject: data, options: [])
-                        let decoder = JSONDecoder()
-                        let user = try decoder.decode(User.self, from: jsonData)
-                        
-                        completion(user)
-                    }
-                } catch let error {
-                    print("Error converting document data to JSON: \(error.localizedDescription)")
-                }
-            } else {
-                print("Document does not exist")
-            }
+    private func setupData()  {
+        viewModel.getData { user in
+            self.mainProfileView.setupUI(userImage: user.userImage, userName: user.userName,
+                                         tombCount: user.tombCount, 
+                                         refrigeratorCount: user.refrigeratorCount,
+                                         hashTag: user.email, isMyProfile: true)
             
         }
         
@@ -149,20 +87,20 @@ extension MyProfileVIewController: UITableViewDelegate, UITableViewDataSource {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "MyProfileTableViewCell", for: indexPath) as? MyProfileTableViewCell {
             
             switch indexPath.row {
-                case 0:
-                    cell.settingCell(iconHidden: true, title: "설정")
-                case 1:
-                    cell.settingCell(iconImageName: "settingUser", title: "계정")
-                case 2:
-                    cell.settingCell(iconImageName: "bell", title: "알림")
-                case 3:
-                    cell.settingCell(iconImageName: "question", title: "문의")
-                case 4:
-                    cell.settingCell(iconImageName: "powerOff", title: "로그아웃")
-                case 5:
-                    cell.settingCell(subInfo: false)
-                default:
-                    break
+            case 0:
+                cell.settingCell(iconHidden: true, title: "설정")
+            case 1:
+                cell.settingCell(iconImageName: "settingUser", title: "계정")
+            case 2:
+                cell.settingCell(iconImageName: "bell", title: "알림")
+            case 3:
+                cell.settingCell(iconImageName: "question", title: "문의")
+            case 4:
+                cell.settingCell(iconImageName: "powerOff", title: "로그아웃")
+            case 5:
+                cell.settingCell(subInfo: false)
+            default:
+                break
             }
             
             return cell
@@ -170,21 +108,16 @@ extension MyProfileVIewController: UITableViewDelegate, UITableViewDataSource {
         
         return UITableViewCell()
     }
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.row {
         case 1:
             let vc = AccountViewController()
             self.navigationController?.pushViewController(vc, animated: true)
         case 4:
-            do {
-                try Auth.auth().signOut()
-                GIDSignIn.sharedInstance.signOut()
-                GIDSignIn.sharedInstance.disconnect()
+            viewModel.authLogout {
                 let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as! SceneDelegate
                 sceneDelegate.moveToSplash()
-            } catch {
-                print("logout error")
             }
         default:
             break
@@ -196,7 +129,6 @@ extension MyProfileVIewController: UITableViewDelegate, UITableViewDataSource {
 
 extension MyProfileVIewController: ProfileMainViewDelegate {
     func profileImageTapped() {
-    
         let vc = MyProfileEditViewController()
         vc.delegate = self
         navigationController?.pushViewController(vc, animated: true)
