9ee8b755df63aa53a024b9249a70a02d2657f3bc
DEV_Ruel <67133244+iOS-Ruel@users.noreply.github.com>
2024-06-13 10:37:09 +0900

    Modi] iamgeView loadImage 수정

--- YGGG/YGGG/Extension/Extension+UIImageView.swift
+++ YGGG/YGGG/Extension/Extension+UIImageView.swift
@@ -10,27 +10,43 @@ import UIKit
 
 extension UIImageView {
     func loadImage(from urlString: String) {
+
         guard let url = URL(string: urlString) else {
-            print("loadImage2   Invalid URL")
+            print("Invalid URL")
             return
         }
         
-        // 비동기적으로 URL에서 이미지를 다운로드
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        let cache = URLCache.shared
+        let request = URLRequest(url: url)
+        
+        // 데이터 검색
+        if let data = cache.cachedResponse(for: request)?.data, let image = UIImage(data: data) {
+            DispatchQueue.main.async {
+                self.image = image
+            }
+            return
+        }
+        
+        // 네트워크 요청
+        URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 print("Error loading image: \(error)")
                 return
             }
             
-            guard let data = data, let image = UIImage(data: data) else {
-                print("No data or failed to create image")
+            guard let data = data, let response = response, let image = UIImage(data: data) else {
+                print("No data , failed to load image")
                 return
             }
             
-            // 메인 스레드에서 UIImageView에 이미지를 설정
+            // 캐시 저장
+            let cachedResponse = CachedURLResponse(response: response, data: data)
+            cache.storeCachedResponse(cachedResponse, for: request)
+            
             DispatchQueue.main.async {
                 self.image = image
             }
+            
         }.resume()
     }
 }
